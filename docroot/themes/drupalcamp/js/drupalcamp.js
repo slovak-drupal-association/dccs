924f442f35b64594be633296bafe01aa9b4b0775
Miroslav Michalicka <miro.michalicka@gmail.com>
2015-05-20 19:46:54 +0200

    Changes related to program and some bugfixing

--- docroot/themes/drupalcamp/js/drupalcamp.js
+++ docroot/themes/drupalcamp/js/drupalcamp.js
@@ -1,17 +1,17 @@
 (function ($, Drupal) {
 
-  Drupal.behaviors.inputButton = {
-    attach: function (context, settings) {
-      $('link[href="//cdn-images.mailchimp.com/embedcode/classic-081711.css"]').remove();
-    }
-  };
+    Drupal.behaviors.inputButton = {
+        attach: function (context, settings) {
+            $('link[href="//cdn-images.mailchimp.com/embedcode/classic-081711.css"]').remove();
+        }
+    };
 
-  Drupal.behaviors.menuToggleDC = {
-    attach: function (context, settings) {
-				$( '.menu-toggle' ).click(function() {
-					$( '#block-drupalcamp-main-menu' ).find( 'ul.menu' ).toggleClass( 'open' );
-				});
-    }
-  };
+    Drupal.behaviors.menuToggleDC = {
+        attach: function (context, settings) {
+            $('.menu-toggle').click(function () {
+                $('#block-drupalcamp-main-menu').find('ul.menu').toggleClass('open');
+            });
+        }
+    };
 
 })(jQuery, Drupal);
